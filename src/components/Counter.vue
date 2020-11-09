7cd2a6102e9287cbbe5c34cae7b10d72610f6a0e
adriano hardtke <adr.hardtke@gmail.com>
2020-11-09 18:19:08 -0300

    before vuex state management

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="decrement">Decrement</button>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data: () => (
+        {
+            counter: 0
+        }
+    ),
+    methods: {
+        decrement: function() {
+            this.counter--;
+        },
+        increment: function(){
+            this.counter++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
